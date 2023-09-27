135a07f945e8904f7a0b41866b077e43f0036011
Francisco Iaconis <franciscoiaconis@gmail.com>
2023-09-27 20:41:10 -0300

    add drive_colab for mounting the drive

--- LCI/utils.py
+++ LCI/utils.py
@@ -1,2 +1,11 @@
 def say_hi(name):
   print(f'Hi, {name}')
+
+def drive_colab():
+  # This lines are for mount the drive and get the auth from google
+  from pydrive.auth import GoogleAuth
+  from pydrive.drive import GoogleDrive
+  from google.colab import auth
+  from oauth2client.client import GoogleCredentials
+  from google.colab import drive
+  drive.mount('/content/drive')

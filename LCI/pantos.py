2874965d86b7442edc97b0569cd1ec12a701a68e
Francisco Iaconis <franciscoiaconis@gmail.com>
2023-09-28 17:27:33 -0300

    fix2 pantos

--- LCI/pantos.py
+++ LCI/pantos.py
@@ -12,14 +12,12 @@ def erase_txt(img, blur = True):
     """
     Process an image by either blurring or covering text regions with black rectangles.
 
-    This function takes an image and the result of applying the Reader method from EasyOCR.
+    This function takes an image.
     If text with a probability greater than 0.5 is detected, it applies blurring or covers
     the text with a black rectangle. If no such text is found, it returns the original image.
 
     Args:
         img (numpy.ndarray): The input image to be processed.
-        res (list): A list of tuples containing text detection results, where each tuple
-                    consists of (bbox, text, prob).
         blur (bool, optional): If True, blur the detected text regions; otherwise, cover
                                them with black rectangles. Default is True.
 
@@ -28,8 +26,7 @@ def erase_txt(img, blur = True):
 
     Example:
         img = cv2.imread("image.jpg")
-        results = reader.readtext(img)
-        processed_img = proc_img(img, results, blur=True)
+        processed_img = proc_img(img, blur=True)
     """
     try:
         from easyocr import Reader
